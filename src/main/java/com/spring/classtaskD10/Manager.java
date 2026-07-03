1b900fb7cee7540378e48a12326085ef0bf86d6f
Yuvraj Singh Kushwah <kushwahyuvraj262@gmail.com>
2026-07-03 23:59:38 +0530

    I have completed class task Day_10

---
+++ src/main/java/com/spring/classtaskD10/Manager.java
@@ -0,0 +1,34 @@
+package com.spring.classtaskD10;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Manager {
+
+	private String managerName;
+	private String department;
+
+	public Manager(String managerName, String department) {
+		this.managerName = managerName;
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "{Manager Name=" + managerName + ", Department=" + department + "}";
+	}
+
+	
+	// This code is just for verification purposes, to check whether the values are
+	// injected into the variables or not
+	
+	  public static void main(String[] args) {
+	  
+	  ApplicationContext ac = new
+	  ClassPathXmlApplicationContext("com/spring/config/application-context.xml");
+	  Manager manager = ac.getBean("manager", Manager.class);
+	  System.out.println(manager);
+	  
+	  }
+	 
+}
